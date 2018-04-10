08396e2a72992eafaaf8b94b06827d231e450a8c
zach <zte.zachary@gmail.com>
2018-04-10 16:03:55 -0700

    Fix navigation.

--- app/src/main/java/com/example/zach/musicians/ui/CreateAccountActivity.java
+++ app/src/main/java/com/example/zach/musicians/ui/CreateAccountActivity.java
@@ -62,20 +62,6 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
         createAuthProgressDialog();
     }
 
-    private void createAuthStateListener() {
-        mAuthListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                final FirebaseUser user = firebaseAuth.getCurrentUser();
-                if (user != null) {
-                    Intent intent = new Intent(CreateAccountActivity.this, MainActivity.class);
-                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                    startActivity(intent);
-                    finish();
-                }
-            }
-        };
-    }
 
     private void createAuthProgressDialog() {
         mAuthProgressDialog = new ProgressDialog(this);
@@ -180,6 +166,20 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
                         }
                     }
                 });
+    }
+    private void createAuthStateListener() {
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                final FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Intent intent = new Intent(CreateAccountActivity.this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
     }
 
     @Override
--- app/src/main/java/com/example/zach/musicians/ui/MainActivity.java
+++ app/src/main/java/com/example/zach/musicians/ui/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.zach.musicians.ui;
 
+import android.support.v4.app.Fragment;
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -11,11 +15,39 @@ import com.example.zach.musicians.R;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String SELECTED_ITEM = "arg_selected_item";
+
+    private BottomNavigationView mBottomNav;
+    private int mSelectedItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBottomNav = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        mBottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                selectFragment(item);
+                return false;
+            }
+        });
+
+        MenuItem selectedItem;
+        if (savedInstanceState != null) {
+            mSelectedItem = savedInstanceState.getInt(SELECTED_ITEM, 0);
+            selectedItem = mBottomNav.getMenu().findItem(mSelectedItem);
+        } else {
+            selectedItem = mBottomNav.getMenu().getItem(0);
+        }
+        selectFragment(selectedItem);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putInt(SELECTED_ITEM, mSelectedItem);
+        super.onSaveInstanceState(outState);
     }
 
     @Override
@@ -33,6 +65,33 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void selectFragment(MenuItem item) {
+        Fragment frag = null;
+        switch (item.getItemId()) {
+
+                case R.id.action_profile:
+                Intent intent = new Intent(MainActivity.this, CreateProfileActivity.class);
+                startActivity(intent);
+                break;
+
+                case R.id.action_search:
+                //something
+                break;
+
+                case R.id.action_messages:
+                break;
+
+
+        }
+        mSelectedItem = item.getItemId();
+
+//        for (int i = 0; i< mBottomNav.getMenu().size(); i++) {
+//            MenuItem menuItem = mBottomNav.getMenu().getItem(i);
+//            menuItem.setChecked(menuItem.getItemId() == item.getItemId());
+//        }
+
     }
 
     private void logout() {
