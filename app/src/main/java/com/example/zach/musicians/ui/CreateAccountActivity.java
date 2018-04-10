025adfdc49443a8167598bbbd2a9880ace64f2e1
zach <zte.zachary@gmail.com>
2018-04-10 10:39:51 -0700

    Add logout menu item to main activity.

--- app/src/main/java/com/example/zach/musicians/ui/CreateAccountActivity.java
+++ app/src/main/java/com/example/zach/musicians/ui/CreateAccountActivity.java
@@ -7,6 +7,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.Patterns;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
