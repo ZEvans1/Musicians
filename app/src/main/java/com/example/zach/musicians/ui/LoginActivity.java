5a17f9e9fa9ebdaf37314624429dfb9e5a6c7ae8
zach <zte.zachary@gmail.com>
2018-04-10 10:53:10 -0700

    Implement user login.

--- app/src/main/java/com/example/zach/musicians/ui/LoginActivity.java
+++ app/src/main/java/com/example/zach/musicians/ui/LoginActivity.java
@@ -6,9 +6,15 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.zach.musicians.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -18,6 +24,9 @@ import butterknife.ButterKnife;
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
 
     @BindView(R.id.signupTextView) TextView mSignupTextView;
+    @BindView(R.id.loginButton) Button mLoginButton;
+    @BindView(R.id.userNameEditText) EditText mUserNameEditText;
+    @BindView(R.id.passwordEditText) EditText mPasswordEditText;
 
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
@@ -44,6 +53,8 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         };
 
         mSignupTextView.setOnClickListener(this);
+        mLoginButton.setOnClickListener(this);
+        createAuthProgressDialog();
     }
 
     @Override
@@ -53,6 +64,43 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             startActivity(intent);
             finish();
         }
+
+        if (view == mLoginButton) {
+            loginWithPassword();
+        }
+    }
+
+    private void loginWithPassword() {
+        String email = mUserNameEditText.getText().toString().trim();
+        String password = mPasswordEditText.getText().toString().trim();
+        if (email.equals("")) {
+            mUserNameEditText.setError("Enter email");
+            return;
+        }
+        if (password.equals("")) {
+            mPasswordEditText.setError("Enter password");
+            return;
+        }
+
+        mAuthProgressDialog.show();
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        mAuthProgressDialog.dismiss();
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(LoginActivity.this, "Auth failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void createAuthProgressDialog() {
+        mAuthProgressDialog = new ProgressDialog(this);
+        mAuthProgressDialog.setTitle("Loading...");
+        mAuthProgressDialog.setMessage("Authenticating with Firebase...");
+        mAuthProgressDialog.setCancelable(false);
     }
 
     @Override
