345eeaac4e69f60524e87d6c0ca9638083f81f1e
zach <zte.zachary@gmail.com>
2018-04-12 11:10:34 -0700

    Allow user name and bio to be shown on user profile fragment.

--- app/src/main/java/com/example/zach/musicians/ui/UserProfileFragment.java
+++ app/src/main/java/com/example/zach/musicians/ui/UserProfileFragment.java
@@ -3,25 +3,75 @@ package com.example.zach.musicians.ui;
 
 import android.os.Bundle;
 import android.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.example.zach.musicians.Constants;
 import com.example.zach.musicians.R;
+import com.example.zach.musicians.models.User;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.parceler.Parcels;
+import org.w3c.dom.Text;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class UserProfileFragment extends Fragment {
+    @BindView(R.id.userNameTextView) TextView mUserNameTextView;
+    @BindView(R.id.userBioTextView) TextView mUserBioTextView;
 
+
+
+    private User mUser;
 
     public UserProfileFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_user_profile, container, false);
+        ButterKnife.bind(this, view);
+
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        FirebaseDatabase currentUserDB = FirebaseDatabase.getInstance();
+        DatabaseReference currentUserRef = currentUserDB.getReference("users")
+                .child(firebaseUser.getUid());
+
+        currentUserRef.child("userName").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String userName = dataSnapshot.getValue(String.class);
+                mUserNameTextView.setText(userName);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {}
+        });
+
+        currentUserRef.child("userBio").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String userBio = dataSnapshot.getValue(String.class);
+                mUserBioTextView.setText(userBio);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {}
+        });
+
+        return view;
     }
 
-}
+    }
